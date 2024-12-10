bda6ade709e43194ab99ee37977db7846463d4eb
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2024-12-10 16:26:33 +0100

    Début de la programmation pour l'affichage des jeux

--- views/homeView.php
+++ views/homeView.php
@@ -2,7 +2,7 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col">
-                <h1>SALUT, <?= htmlspecialchars($_SESSION['user']['username']) ?> ! <br> PRÊT À AJOUTER DES JEUX À TA COLLECTION ?</h1>
+                <h1>SALUT, <?php htmlspecialchars($_SESSION['user']['username']) ?> ! <br> PRÊT À AJOUTER DES JEUX À TA COLLECTION ?</h1>
             </div>
         </div>
         <div class="row">
@@ -10,26 +10,8 @@
                 <h2>MES JEUX</h2>
             </div>
         </div>
-
-        <!---
-        <div class="row">
-            <?php if ($result->num_rows > 0): ?>
-                <?php while ($row = $result->fetch_assoc()):?>
-                    <div class="col-md-4">
-                        <div class="card mb-4">
-                            <img src="<?= htmlspecialchars($row['couverture']) ?>" class="card-img-top" alt="<?= htmlspecialchars($row['nomJV']) ?>">
-                            <div class="card-body">
-                                <h5 class="card-title"><?= htmlspecialchars($row['nomJV']) ?></h5>
-                                <p class="card-text">Éditeur: <?= htmlspecialchars($row['editeur']) ?></p>
-                                <p class="card-text">Date de sortie: <?= htmlspecialchars($row['dateSortie']) ?></p>
-                            </div>
-                        </div>
-                    </div>
-                <?php endwhile; ?>
-            <?php else: ?>
-                <p class="text-center">Aucun jeu trouvé dans la ludothèque.</p>
-            <?php endif; ?>
-        </div>
-        --->
+        <?php foreach($jeux as $jeu) ?>
+        
+        <?php endforeach; ?>
     </div>
 </main>
