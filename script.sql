9f55dcb6ae90d87b58d16e931d68f3a8b2a736f9
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2024-12-10 14:56:54 +0100

    Upload du script de création

---
+++ script.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS utilisateur;
+DROP TABLE IF EXISTS jeu_video;
+DROP TABLE IF EXISTS bibliotheque;
+
+CREATE TABLE utilisateur(
+    idUser INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    prenom VARCHAR(50),
+    nom VARCHAR(50),
+    email VARCHAR(50),
+    mdp VARCHAR(150)
+    );
+    
+CREATE TABLE jeu_video(
+	idJV INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomJV VARCHAR(50),
+    editeur VARCHAR(50),
+    dateSortie DATE,
+    couverture VARCHAR(50)
+);
+
+
+CREATE TABLE bibliotheque(
+    idUser INT, 
+    idJV INT,
+	FOREIGN KEY (idUser) REFERENCES utilisateur(idUser),
+	FOREIGN KEY (idJV) REFERENCES jeu_video(idJV),
+    nbHeure INT
+);
+
+
+
+
+
+
