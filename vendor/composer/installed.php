4e990a7d279df7a6b27a4ef1dc4d8a381f54d27e
MattxdGithub <mattxd22@gmail.com>
2024-12-10 15:16:08 +0100

    ajout vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'projet/game-collection',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2ac95f4bb788b02bf2c21216c391afcd087c418a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projet/game-collection' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2ac95f4bb788b02bf2c21216c391afcd087c418a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
