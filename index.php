2ccd2f3aa062591b5d4cd2789f997026e08643f4
tatannjs <ethan.eveilleaux@gmail.com>
2024-12-10 16:29:15 +0100

    petite redirection

--- index.php
+++ index.php
@@ -2,12 +2,31 @@
 
 include "vendor/autoload.php";
 
-
-/* Structure de base d'une page*/
 include("views/headerView.php");
 
-include("views/homeView.php");
+// Analyser l'URL pour déterminer la vue à charger
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case '/' :
+    case '' :
+        include('views/homeView.php');
+        break;
+    case '/bibliotheque' :
+        #loadView('bibliotheque');
+        break;
+    case '/ajout' :
+        #loadView('ajout');
+        break;
+    case '/classement' :
+        #loadView('classements');
+        break;
+    case '/profil' :
+        #loadView('profil');
+        break;
+    default:
+        #loadView('404');
+        break;
+}
 
 include("views/footerView.php");
-
 ?>
