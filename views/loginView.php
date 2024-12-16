f74e34de0349e7867de30be43a8579b79e40f7f2
MattxdGithub <mattxd22@gmail.com>
2024-12-16 12:00:27 +0100

    config  github

--- views/loginView.php
+++ views/loginView.php
@@ -4,13 +4,13 @@
         <title>Login</title>
         <meta charset="utf-8" lang="fr">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/styleLogin.css">
+        <link rel="stylesheet" href="../assets/styleLogin.css">
     </head>
     <body>
-        <div class="container">
+        <div class="container ">
             <h2>Se connecter à Game Collection</h2>
         </div>
     </body>
-<?php include 'views/footer.php'; ?>
+<?php include 'footer.php'; ?>
 </html>
     
