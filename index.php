966866153d004781469b0f726fe082c42c13c11f
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2025-01-02 13:57:24 +0100

    débugage index.php

--- index.php
+++ index.php
@@ -27,6 +27,8 @@ switch ($request) {
         break;   
     case '/home':
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            var_dump($_POST); 
+            die(); 
             $email = $_POST['email'];
             $password = $_POST['password'];
             $user = loginModels::findByEmail($email);
