fd2ef2d3dfbc0da81a4ec7fb5fc69acabff74475
MattxdGithub <mattxd22@gmail.com>
2025-01-11 18:20:29 +0100

    test 2 connexion bdd

--- models/bddModels.php
+++ models/bddModels.php
@@ -1,15 +1,20 @@
 <?php
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 if (!function_exists('create_bdd')) {
-function create_bdd(){
-    $host = $_ENV["DBHOST"];
-    $dbname = $_ENV["DBNAME"];
-    $user = $_ENV["DBUSER"];
-    $password = $_ENV["DBMDP"];
-    $pdo = new PDO('mysql:host='.$host.'; dbname='. $dbname, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-    return $pdo;
-}
+    function create_bdd(){
+        $host = $_ENV["DBHOST"];
+        $dbname = $_ENV["DBNAME"];
+        $user = $_ENV["DBUSER"];
+        $password = $_ENV["DBMDP"];
+        $pdo = new PDO('mysql:host='.$host.'; dbname='. $dbname, $user, $password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        return $pdo;
+    }
 }
 
 ?>
