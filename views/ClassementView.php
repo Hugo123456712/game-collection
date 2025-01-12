95c226f614e095fab1be94974c67b98bd316fb68
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2025-01-12 21:35:59 +0100

    Tentative 4

--- views/ClassementView.php
+++ views/ClassementView.php
@@ -13,7 +13,7 @@ $user = $_SESSION['user'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classement</title>
-    <link rel="stylesheet" href="/assets/classementViewStyle.css">
+    <link rel="stylesheet" href="/assets/ClassementViewStyle.css">
 </head>
 <body>
     <?php include("headerView.php"); ?>
