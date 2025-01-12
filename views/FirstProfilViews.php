de3762458f183f712f3a689939749621d706b07e
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2025-01-12 22:35:19 +0100

    test 3

--- views/FirstProfilViews.php
+++ views/FirstProfilViews.php
@@ -23,7 +23,7 @@ $user = $_SESSION['user'];
         <p class="nom"><strong>Nom :</strong> <?= htmlspecialchars($user['nom']) ?></p>
         <p class="prenom"><strong>Prénom :</strong> <?= htmlspecialchars($user['prenom']) ?></p>
         <p class="email"><strong>Email :</strong> <?= htmlspecialchars($user['email']) ?></p>
-        <form method="post" action="/profil">
+        <form action="/profil">
             <button type="submit" class="btn btn-primary">Modifier mon profil</button>
         </form>
         <form method="post" action="/deleteAccount">
