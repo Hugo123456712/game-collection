ce17b8e0ab2718c3247d0a3cea2bc376cfca1ab2
MattxdGithub <mattxd22@gmail.com>
2025-01-12 17:27:48 +0100

    modif css login

--- views/loginView.php
+++ views/loginView.php
@@ -11,14 +11,14 @@
         <?php endif; ?>
         <form method="POST" action="/login">
             <div class="email-input">
-                <label for="email">Email</label>
+                <label for="email">Email :</label>
                 <input type="email" id="email" name="email" required>
             </div>
             <div class="mdp">
-                <label for="mdp">Mot de passe</label>
+                <label for="mdp">Mot de passe :</label>
                 <input type="password" id="mdp" name="mdp" required>
             </div>
-            <input type="submit" value="Se connecter">
+            <input type="submit" value="SE CONNECTER">
         </form>
         <p class="redirect"><a href="/signup">S'inscrire</a></p>
     </div>
